7d45e1c9b9653e653cdfd65a831c960fb8a30160
TrickyCmd <info@viraledge.co.uk>
2018-10-03 20:58:02 +0100

    chore: removing old CMS code and redundant components

--- site/themes/materialize/components/misc/half-height-hero/index.js
+++
@@ -1,6 +0,0 @@
-// const halfHeightHero = {
-//   titleText: 'Westminster Associates',
-//   dynamicText: 'Inspiring People to ',
-//   dynamicWords: 'Connect,Grow,Perform',
-//   dynamicWordColors: '#CA6560,#CDDE79,#1F8CA5'
-// }
